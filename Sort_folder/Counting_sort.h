3feb4e97476906bcb5ba844d74d50254fbc4332c
KMA-K <baoanisa0001@gmail.com>
2025-04-26 10:43:42 +0700

    Update Counting_sort.h

--- Sort_folder/Counting_sort.h
+++ Sort_folder/Counting_sort.h
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
+#pragma once
 #include <iostream>
 #include <vector>
 #include <algorithm>
-=======
-#pragma once
-#include<iostream>
-#include<vector>
->>>>>>> 595a8ea (Cập nhật tất cả các thay đổi)
+
 using namespace std;
+
 long long countSort(vector<long long>& inputArray)
 {
     long long comparison = 0;
     int N = inputArray.size();
-<<<<<<< HEAD
-
-    
-=======
->>>>>>> 595a8ea (Cập nhật tất cả các thay đổi)
     int M = 0;
 
     for (int i = 0; i < N; i++) {
@@ -24,36 +16,18 @@ long long countSort(vector<long long>& inputArray)
         comparison++;
     }
 
-<<<<<<< HEAD
-    
-    vector<int> countArray(M + 1, 0);
-
-    
-
-    for (int i = 0; i < N; i++)
-=======
     vector<int> countArray(M + 1, 0);
 
     for (int i = 0; i < N; i++) {
->>>>>>> 595a8ea (Cập nhật tất cả các thay đổi)
         countArray[inputArray[i]]++;
         comparison++;
     }
 
-<<<<<<< HEAD
-    
-    for (int i = 1; i <= M; i++)
-=======
     for (int i = 1; i <= M; i++) {
->>>>>>> 595a8ea (Cập nhật tất cả các thay đổi)
         countArray[i] += countArray[i - 1];
         comparison++;
     }
 
-<<<<<<< HEAD
-    
-=======
->>>>>>> 595a8ea (Cập nhật tất cả các thay đổi)
     vector<int> outputArray(N);
 
     for (int i = N - 1; i >= 0; i--) {
