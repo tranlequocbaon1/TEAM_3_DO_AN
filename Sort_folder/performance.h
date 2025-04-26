88a3334bd132ac9ce0ee12d560318fc18e11b359
KMA-K <baoanisa0001@gmail.com>
2025-04-26 10:32:29 +0700

    Update performance.h

--- Sort_folder/performance.h
+++ Sort_folder/performance.h
@@ -8,10 +8,10 @@ using namespace std::chrono;
 // second= time_complete 
 pair<long long , long long> Performance(vector<long long>v,long long(*function)(vector<long long>&)){
 	vector<long long> a=v;
-	auto start=high_resolution_clock::now();
+	auto start=high_resolution_clock::now();// bắc đầu bấm giờ
 	long long count=function(a);
-	auto end=high_resolution_clock::now();
-	long long time_complete=duration_cast<milliseconds>(end-start).count();
+	auto end=high_resolution_clock::now();// bấm đừng
+	long long time_complete=duration_cast<milliseconds>(end-start).count();// gán giá trị
 	pair<long long, long long> ans={count,time_complete};
 	return ans;
 		
